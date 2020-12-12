1ac7258907de5e2927fc6bbc5f14f9036da907a2
Alex <alexilin83@gmail.com>
2020-12-12 15:07:48 +0300

    update dep

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,5 +1,3 @@
-/** @type {import("snowpack").SnowpackUserConfig } */
-
 module.exports = {
   mount: {
     public: '/',
@@ -8,34 +6,8 @@ module.exports = {
   plugins: [
     '@snowpack/plugin-svelte',
     '@snowpack/plugin-webpack',
-    [
-      'snowpack-plugin-imagemin',
-      {
-        /* see "Plugin Options" below */
-        include: ['**/*.jpg', '**/*.png'],
-        plugins: [
-          require('imagemin-mozjpeg')({quality: 90, progressive: true}),
-          require('imagemin-optipng')({optimizationLevel: 7}),
-        ],
-      },
-    ],
   ],
-  install: [
-    /* ... */
-  ],
-  installOptions: {
-    /* ... */
-  },
-  devOptions: {
-    /* ... */
-  },
   buildOptions: {
     clean: true
-  },
-  proxy: {
-    /* ... */
-  },
-  alias: {
-    /* ... */
-  },
+  }
 };
